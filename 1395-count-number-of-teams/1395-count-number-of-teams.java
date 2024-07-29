0c8a1acb6a218dc6cef0c38c545b5df7a5f49eec
Sharanparamesh7 <sharanparamesh860@gmail.com>
2024-07-29 14:58:10 +0530

    Time: 2312 ms (5.07%), Space: 42.5 MB (38.54%) - LeetHub

---
+++ 1395-count-number-of-teams/1395-count-number-of-teams.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numTeams(int[] r) {
+        int count = 0;
+        for(int i=0; i<r.length; i++){
+            for(int j=i+1; j<r.length; j++){
+                for(int k=j+1; k<r.length; k++){
+                    if(r[i] < r[j] && r[j] < r[k] && r[i] < r[k]){
+                        count ++;
+                    }
+                    else if(r[i] > r[j] && r[j] > r[k] && r[i] > r[k]){
+                        count ++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
