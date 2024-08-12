c9262b64fe895eab0ff56cfccb90c85cc4ee2da9
Sharanparamesh7 <sharanparamesh860@gmail.com>
2024-08-12 20:31:02 +0530

    Time: 10 ms (14.54%), Space: 44.9 MB (25.22%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,75 @@
+class Solution
+{
+     public void solve(int n,int col,char[][] board,List<List<String>> res)
+    {
+        if(col==n)
+        {
+            List<String> list = new ArrayList<>();
+            for(int i=0; i<n ; i++)
+            {
+                String str=""; 
+                for(int j=0;j<n;j++)
+                {
+                    str = str+board[i][j];
+                }
+                list.add(str);
+            } 
+            res.add(new ArrayList<>(list));
+            return;           
+        }
+        else 
+        {
+            for(int row=0 ; row<n ; row++)
+            { 
+                if(issSafe(row,col,n,board)==true)
+                {
+                    board[row][col] = 'Q'; 
+                    solve(n,col+1,board,res); 
+                    board[row][col] = '.';
+                }
+            }
+        }
+    }
+    public boolean issSafe(int row,int col,int n,char[][] board)
+    {
+        int i,j; 
+        for(j=col-1 ; j>=0 ; j--)
+        {
+            if(board[row][j]=='Q')
+            {
+                return false;
+            }
+        } 
+        for(i=row-1,j=col-1 ; i>=0&&j>=0 ; i--,j--)
+        {
+            if(board[i][j]=='Q')
+            {
+                return false;
+            }
+        }
+        for(i=row+1,j=col-1 ; i<=n-1&&j>=0 ; i++,j--)
+        {
+            if(board[i][j]=='Q')
+            {
+                return false;
+            }
+        } 
+        return true;
+    }
+   
+    public List<List<String>> solveNQueens(int n)
+    { 
+        List<List<String>> res = new ArrayList<>(); 
+        char[][] board = new char[n][n]; 
+        int i,j; 
+        for(i=0 ; i<n ; i++)
+        {
+            for(j=0 ; j<n ; j++) 
+            {
+                board[i][j] = '.';
+            }
+        }
+        solve(n,0,board,res); 
+        return res;
+    }
+}
